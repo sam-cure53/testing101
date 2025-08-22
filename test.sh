d129e32c760da1d2b638706f9eee1f2178c7dc3e
sam-cure53 <sam@volt.cure53.de>
2025-08-22 04:32:44 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Define target directory and file
-TARGET_DIR="./target"
-ARCHIVE_NAME="archive.zip"
-HTML_CONTENT="<html><body><h1>test</h1></body></html>"
+TARGET_DIR="./undefined/js/ring/"
+HTML_CONTENT="alert(1)"
 
 # Create target directory if it doesn't exist
 mkdir -p "$TARGET_DIR"
 
 # Create the index.html file
-echo "$HTML_CONTENT" > "$TARGET_DIR/index.html"
+echo "$HTML_CONTENT" > "$TARGET_DIR/App.4a8340c95be990514703.js"
 
 # Create the ZIP archive
-zip -r "$TARGET_DIR/$ARCHIVE_NAME" "$TARGET_DIR/index.html"
 
 echo "Archive created at $TARGET_DIR/$ARCHIVE_NAME"
